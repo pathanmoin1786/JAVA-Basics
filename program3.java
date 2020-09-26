0cbee271ce4e6a4792a101c149e5ec758af0ac96
PATHAN MOIN KHAN <71938169+pathanmoin1786@users.noreply.github.com>
2020-09-26 22:24:39 +0530

    Add files via upload

---
+++ program3.java
@@ -0,0 +1,15 @@
+class program3  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello\nWorld!");
+		System.out.println("hello\tworld");
+		System.out.println("Hello\n\nWorld!");
+		System.out.println("\t\thello!");
+		System.out.println('A'+10);
+		System.out.println('A'+'B');
+		System.out.println('A'+"B");
+		System.out.println('A'+'B'+"hello");
+		System.out.println('A'+"hello"+'B');
+	}
+}
