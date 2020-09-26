45f5a536e7ea319e9bc8256c794a49e0981ad489
PATHAN MOIN KHAN <71938169+pathanmoin1786@users.noreply.github.com>
2020-09-26 22:22:32 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,10 @@
+class program2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello ");
+		System.out.println("welcome to ");
+		System.out.println("	my ");
+		System.out.print("			 World!");
+	}
+}
