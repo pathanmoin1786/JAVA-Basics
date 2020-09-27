b7cd237166a2e4f9eed6fa54205ebd220f7725cc
PATHAN MOIN KHAN <71938169+pathanmoin1786@users.noreply.github.com>
2020-09-27 10:43:10 +0530

    Add files via upload

---
+++ variabledeclaration.java
@@ -0,0 +1,24 @@
+class variabledeclaration 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("program starts here");
+		//variable declaration
+		int num1;
+		double d;
+		char ch;
+		boolean b;
+		//variable intialization 
+		num1=100;
+		d=10.24;
+		ch='A';
+		b=false;
+		System.out.println("num1="+num1);
+		System.out.println("d="+d);
+		System.out.println("ch="+ch);
+		System.out.println("b="+b);
+		System.out.println("here the program ends");
+
+		
+	}
+}
